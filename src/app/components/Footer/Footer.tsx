f74eb86f82bae0537386ccb8c7f2e4c5f96a5d92
sturquier <turquiersimon@hotmail.fr>
2024-07-22 14:07:55 +0200

    WIP : responsive footer/header & markdown pages

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -39,35 +39,39 @@ export default function Footer(): JSX.Element {
   return (
     <footer className='footer'>
       <div className='footer-journal'>
-        <div className='footer-journal-logo'>
-          <img src={logoJpeSmall} alt='Journal logo'/>
-          {getJournalNotice() && <Link to={getJournalNotice()!} target='_blank'>{t('components.footer.links.notice')}</Link>}
-          <div>|</div>
-          {getContact() && <Link to={getContact()!} target='_blank'>{t('components.footer.links.contact')}</Link>}
-          <div>|</div>
-          <Link to={PATHS.credits}>{t('components.footer.links.credits')}</Link>
-        </div>
-        <div className='footer-journal-rss'>
-          {getISSN() && <div>{`eISSN ${getISSN()}`}</div>}
-          {getISSN() && <div>|</div>}
-          {getRSS() && <Link to={getRSS()!} target='_blank'>{t('components.footer.links.rss')}</Link>}
+        <img src={logoJpeSmall} alt='Journal logo' className='footer-journal-logo' />
+        <div className='footer-journal-links'>
+          <div className='footer-journal-links-journal'>
+            {getJournalNotice() && <Link to={getJournalNotice()!} target='_blank'>{t('components.footer.links.notice')}</Link>}
+            <div className='footer-journal-links-journal-divider'>|</div>
+            {getContact() && <Link to={getContact()!} target='_blank'>{t('components.footer.links.contact')}</Link>}
+            <div className='footer-journal-links-journal-divider'>|</div>
+            <Link to={PATHS.credits}>{t('components.footer.links.credits')}</Link>
+          </div>
+          <div className='footer-journal-links-rss'>
+            {getISSN() && <div>{`eISSN ${getISSN()}`}</div>}
+            {getISSN() && <div className='footer-journal-links-rss-divider'>|</div>}
+            {getRSS() && <Link to={getRSS()!} target='_blank'>{t('components.footer.links.rss')}</Link>}
+          </div>
         </div>
       </div>
       <div className='footer-episciences'>
-        <div className='footer-episciences-logo'>
-          <img src={logo} alt='Episciences logo'/>
-          <Link to={import.meta.env.VITE_EPISCIENCES_DOCUMENTATION_PAGE} target='_blank'>{t('components.footer.links.documentation')}</Link>
-          <div>|</div>
-          <Link to={import.meta.env.VITE_EPISCIENCES_ACKNOWLEDGEMENTS_PAGE} target='_blank'>{t('components.footer.links.acknowledgements')}</Link>
-          <div>|</div>
-          <Link to={`${PATHS.about}#publishing-policy`}>{t('components.footer.links.publishingPolicy')}</Link>
-        </div>
-        <div className='footer-episciences-legal'>
-          <Link to={import.meta.env.VITE_EPISCIENCES_LEGAL_TERMS_PAGE} target='_blank'>{t('components.footer.links.legalMentions')}</Link>
-          <div>|</div>
-          <Link to={import.meta.env.VITE_EPISCIENCES_LEGAL_PRIVACY_STATEMENT_PAGE} target='_blank'>{t('components.footer.links.privacyStatement')}</Link>
-          <div>|</div>
-          <Link to={import.meta.env.VITE_EPISCIENCES_LEGAL_PRIVACY_TERMS_OF_USE_PAGE} target='_blank'>{t('components.footer.links.termsOfUse')}</Link>
+        <img src={logo} alt='Episciences logo' className='footer-episciences-logo' />
+        <div className='footer-episciences-links'>
+          <div className='footer-episciences-links-documentation'>
+            <Link to={import.meta.env.VITE_EPISCIENCES_DOCUMENTATION_PAGE} target='_blank'>{t('components.footer.links.documentation')}</Link>
+            <div className='footer-episciences-links-documentation-divider'>|</div>
+            <Link to={import.meta.env.VITE_EPISCIENCES_ACKNOWLEDGEMENTS_PAGE} target='_blank'>{t('components.footer.links.acknowledgements')}</Link>
+            <div className='footer-episciences-links-documentation-divider'>|</div>
+            <Link to={`${PATHS.about}#publishing-policy`}>{t('components.footer.links.publishingPolicy')}</Link>
+          </div>
+          <div className='footer-episciences-links-legal'>
+            <Link to={import.meta.env.VITE_EPISCIENCES_LEGAL_TERMS_PAGE} target='_blank'>{t('components.footer.links.legalMentions')}</Link>
+            <div className='footer-episciences-links-legal-divider'>|</div>
+            <Link to={import.meta.env.VITE_EPISCIENCES_LEGAL_PRIVACY_STATEMENT_PAGE} target='_blank'>{t('components.footer.links.privacyStatement')}</Link>
+            <div className='footer-episciences-links-legal-divider'>|</div>
+            <Link to={import.meta.env.VITE_EPISCIENCES_LEGAL_PRIVACY_TERMS_OF_USE_PAGE} target='_blank'>{t('components.footer.links.termsOfUse')}</Link>
+          </div>
         </div>
       </div>
     </footer>
