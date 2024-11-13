1e97df365f1a0e6f0f94617ef0986a75a8c6bb54
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2024-11-13 12:04:36 +0100

    make the loader use the primary color
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- src/app/components/Loader/Loader.tsx
+++ src/app/components/Loader/Loader.tsx
@@ -1,14 +1,15 @@
 import { TailSpin } from "react-loader-spinner"
-
 import './Loader.scss'
 
 export default function Loader(): JSX.Element {
-  return (
-    <div className="loader">
-      <TailSpin
-        color="#7A020D"
-        width={60}
-      />
-    </div>
-  )
+    const primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary').trim();
+
+    return (
+        <div className="loader">
+            <TailSpin
+                color={primaryColor || "#000000"} // Fallback to #000000 if --primary isn't defined
+                width={60}
+            />
+        </div>
+    );
 }
