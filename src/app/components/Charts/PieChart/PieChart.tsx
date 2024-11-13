0ac3477c6d97b3f9b122a2762ac7db70e4569a97
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2024-11-13 12:20:15 +0100

    meke the Journal statistics page chart colors configurable instead of hard coded
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- src/app/components/Charts/PieChart/PieChart.tsx
+++ src/app/components/Charts/PieChart/PieChart.tsx
@@ -1,63 +1,69 @@
 import { TFunction } from 'i18next';
 import {
-	Cell,
-	Pie,
-	PieChart as RechartsPieChart,
-  ResponsiveContainer,
+    Cell,
+    Pie,
+    PieChart as RechartsPieChart,
+    ResponsiveContainer,
 } from 'recharts';
-
 import { IStatValueDetailsAsPieChart } from '../../../../types/stat';
-import './PieChart.scss'
+import './PieChart.scss';
 
 interface IPieChartProps {
-  t: TFunction<"translation", undefined>
-  data: IStatValueDetailsAsPieChart[];
+    t: TFunction<"translation", undefined>;
+    data: IStatValueDetailsAsPieChart[];
 }
 
 export default function PieChart({ t, data }: IPieChartProps): JSX.Element {
-  const CHART_COLORS = ['#9A312C', '#C9605B', '#FF9994', '#FFC9C7'];
+    // Dynamically load the chart colors from the environment
+    const CHART_COLORS = [
+        import.meta.env.VITE_JOURNAL_STATISTICS_COLORS_0 || '#9A312C',
+        import.meta.env.VITE_JOURNAL_STATISTICS_COLORS_1 || '#C9605B',
+        import.meta.env.VITE_JOURNAL_STATISTICS_COLORS_2 || '#FF9994',
+        import.meta.env.VITE_JOURNAL_STATISTICS_COLORS_3 || '#FFC9C7',
+    ];
 
-  const getLegend = (): JSX.Element => {
-    const notBeingToPublishStatuses = data.filter(singleData => !singleData.isBeingToPublishStatus)
-    const beingToPublishStatuses = data.filter(singleData => singleData.isBeingToPublishStatus)
+
+    const getLegend = (): JSX.Element => {
+        const notBeingToPublishStatuses = data.filter(singleData => !singleData.isBeingToPublishStatus);
+        const beingToPublishStatuses = data.filter(singleData => singleData.isBeingToPublishStatus);
+
+        return (
+            <div className='pieChart-legend'>
+                <div className='pieChart-legend-rows'>
+                    {notBeingToPublishStatuses.map((singleData, index) => getLegendRow(singleData, index, CHART_COLORS.slice(0, 2)))}
+                </div>
+                <div className='pieChart-legend-category'>{t('pages.statistics.statuses.beingPublished')}</div>
+                <div className='pieChart-legend-rows'>
+                    {beingToPublishStatuses.map((singleData, index) => getLegendRow(singleData, index, CHART_COLORS.slice(2, 4)))}
+                </div>
+            </div>
+        );
+    };
+
+    const getLegendRow = (singleData: IStatValueDetailsAsPieChart, index: number, colors: string[]): JSX.Element => {
+        return (
+            <div key={index} className='pieChart-legend-rows-row'>
+                <div className='pieChart-legend-rows-row-square' style={{ backgroundColor: colors[index % colors.length]}}></div>
+                <div>{`${singleData.count} ${t(`pages.statistics.statuses.${singleData.status}`)}`}</div>
+            </div>
+        );
+    };
 
     return (
-      <div className='pieChart-legend'>
-        <div className='pieChart-legend-rows'>
-          {notBeingToPublishStatuses.map((singleData, index) => getLegendRow(singleData, index, CHART_COLORS.slice(0, 2)))}
+        <div className='pieChart'>
+            <ResponsiveContainer>
+                <RechartsPieChart>
+                    <Pie dataKey="count" data={data}>
+                        {data.map((_, index) => (
+                            <Cell
+                                key={`cell-${index}`}
+                                fill={CHART_COLORS[index % CHART_COLORS.length]}
+                            />
+                        ))}
+                    </Pie>
+                </RechartsPieChart>
+            </ResponsiveContainer>
+            {getLegend()}
         </div>
-        <div className='pieChart-legend-category'>{t('pages.statistics.statuses.beingPublished')}</div>
-        <div className='pieChart-legend-rows'>
-          {beingToPublishStatuses.map((singleData, index) => getLegendRow(singleData, index, CHART_COLORS.slice(2, 4)))}
-        </div>
-      </div>
-    )
-  }
-
-  const getLegendRow = (singleData: IStatValueDetailsAsPieChart, index: number, colors: string[]): JSX.Element => {
-    return (
-      <div key={index} className='pieChart-legend-rows-row'>
-        <div className='pieChart-legend-rows-row-square' style={{ backgroundColor: colors[index % colors.length]}}></div>
-        <div>{`${singleData.count} ${t(`pages.statistics.statuses.${singleData.status}`)}`}</div>
-      </div>
-    )
-  }
-
-  return (
-    <div className='pieChart'>
-      <ResponsiveContainer>
-        <RechartsPieChart>
-          <Pie dataKey="count" data={data}>
-            {data.map((_, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={CHART_COLORS[index % CHART_COLORS.length]}
-              />
-            ))}
-          </Pie>
-        </RechartsPieChart>
-      </ResponsiveContainer>
-      {getLegend()}
-    </div>
-  )
+    );
 }
