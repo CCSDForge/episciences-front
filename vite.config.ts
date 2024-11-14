54360572800ce36acedc66ae986fd32b29235b42
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2024-11-14 17:47:05 +0100

    create more chunks for fat packages
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,38 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
           @import "src/style/breakpoints.scss";
         `
-      }
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Split vendor dependencies (node_modules) into a separate chunk
-          if (id.includes('node_modules')) {
-            return 'vendor'; // All node_modules go into the "vendor" chunk
-          }
-
-          // Optionally, customize this further to separate other large modules
-          // Example: Split a specific module into its own chunk
-          //if (id.includes('src/components')) {
-          //  return 'components';
-          //}
+            }
         }
-      }
     },
-    chunkSizeWarningLimit: 1000 // Optional: increase the chunk size limit if needed
-  }
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        // Group larger dependencies by category
+                        // See https://bundlephobia.com/
+                        if (id.includes('recharts')) {
+                            return 'recharts';
+                        } else if (id.includes('@citation-js/plugin-csl')) {
+                            return 'plugin-csl';
+                        } else if (id.includes('citation-js')) {
+                            return 'citation-js';
+                        } else {
+                            return 'vendor'; // General vendor chunk
+                        }
+                    }
+                }
+            }
+        },
+        chunkSizeWarningLimit: 500 // Lowered limit for testing smaller chunks
+    }
 })
