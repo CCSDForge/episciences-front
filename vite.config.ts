49dfad7a2974b29d0da7b3d6a96bb670f48a5dbe
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2024-11-15 10:04:28 +0100

    - added favicons - added preconnect dns-prefetch to API and CDN for mathjax
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,50 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `
-          @import "src/style/breakpoints.scss";
-        `
-            }
-        }
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        // Group larger dependencies by category
-                        // See https://bundlephobia.com/
-                        if (id.includes('recharts')) {
-                            return 'recharts';
-                        } else if (id.includes('@citation-js/plugin-csl')) {
-                            return 'plugin-csl';
-                        } else if (id.includes('citation-js')) {
-                            return 'citation-js';
-                        } else {
-                            return 'vendor'; // General vendor chunk
-                        }
-                    }
-                }
-            }
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            react(),
+            {
+                name: 'html-transform',
+                transformIndexHtml(html) {
+                    return html
+                        .replace(
+                            /<!-- ApiDomainPlaceholder -->/g,
+                            `${env.VITE_API_DOMAIN}`
+                        )
+                        .replace(
+                            '<!-- CoarInboxUrlPlaceholder -->',
+                            `${env.VITE_COAR_NOTIFY_INBOX_URL}`
+                        );
+                },
+            },
+        ],
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `
+            @import "src/style/breakpoints.scss";
+          `,
+                },
+            },
         },
-        chunkSizeWarningLimit: 500 // Lowered limit for testing smaller chunks
-    }
-})
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            if (id.includes('recharts')) return 'recharts';
+                            if (id.includes('@citation-js/plugin-csl')) return 'plugin-csl';
+                            if (id.includes('citation-js')) return 'citation-js';
+                            return 'vendor';
+                        }
+                    },
+                },
+            },
+            chunkSizeWarningLimit: 500,
+        },
+    };
+});
