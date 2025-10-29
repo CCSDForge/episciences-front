b94681a027817d82c3aa7eabbf0a0bd510f697b6
Raphaël Tournoy <rtournoy@users.noreply.github.com>
2025-10-29 17:54:13 +0100

    - fixed undefined date - fixed boolean query for authors API
    Signed-off-by: Raphaël Tournoy <rtournoy@users.noreply.github.com>

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,16 @@
 import { AvailableLanguage } from "./i18n";
 
 export const formatDate = (dateString: string, language: AvailableLanguage, overridedOptions?: Intl.DateTimeFormatOptions): string => {
+  if (!dateString) {
+    return '';
+  }
+
   const date = new Date(dateString);
-  
+
+  if (isNaN(date.getTime())) {
+    return '';
+  }
+
   const options: Intl.DateTimeFormatOptions = {
       year: 'numeric',
       month: 'long',
