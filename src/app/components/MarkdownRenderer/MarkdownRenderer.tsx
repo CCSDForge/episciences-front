517140de66c96fcec6429690213150443a2b1c44
ZHAO <zhaoclaire11@gmail.com>
2026-05-27 16:28:09 +0200

    parser html inline markdown

--- src/app/components/MarkdownRenderer/MarkdownRenderer.tsx
+++ src/app/components/MarkdownRenderer/MarkdownRenderer.tsx
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 import './MarkdownRenderer.scss';
+import rehypeRaw from "rehype-raw";
 
 type MarkdownRendererProps = Omit<
   ComponentProps<typeof ReactMarkdown>,
@@ -15,7 +16,7 @@ export default function MarkdownRenderer({
 }: MarkdownRendererProps): JSX.Element {
   return (
     <div className="markdownRenderer">
-      <ReactMarkdown remarkPlugins={[remarkGfm]} {...props}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}  rehypePlugins={[rehypeRaw]}  {...props}>
         {children}
       </ReactMarkdown>
     </div>
